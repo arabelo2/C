ff62b3daee7f6ac5d4d0f8fc23587fa1df96f799
Alexandre RABELO <rabelo_alexandre@hotmail.com>
2020-04-13 20:27:13 -0300

    Create a small test program

---
+++ src/hw.c
@@ -0,0 +1,9 @@
+/*
+  A "Hello, World!" program generally is a computer program that outputs or displays the message "Hello, World!".
+*/
+
+# include <stdio.h>
+int main(){
+    printf("Hello, World!\n");
+    return 0;
+}
