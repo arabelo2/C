59eba527aee2de8033f54e8a4f6e4c4e3d51ad89
Alexandre RABELO <rabelo_alexandre@hotmail.com>
2020-04-15 20:31:15 -0300

    Add name and date

--- src/hw.c
+++ src/hw.c
@@ -1,5 +1,6 @@
-/*
-  A "Hello, World!" program generally is a computer program that outputs or displays the message "Hello, World!".
+/* A "Hello, World!" program generally is a computer program that outputs or displays the message "Hello, World!".
+   Alexandre RABELO
+   April, 13, 2020
 */
 
 # include <stdio.h>
