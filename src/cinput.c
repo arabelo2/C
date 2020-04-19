ec0318e3df8675d680911bb619f115e257b2f3a1
Alexandre RABELO <rabelo_alexandre@hotmail.com>
2020-04-19 19:46:02 -0300

    Add aditional comments to explain why the semicolon is on a separate line

--- src/cinput.c
+++ src/cinput.c
@@ -12,8 +12,12 @@ int main (void){
        prefix operators (++nc)
        postfix operators (nc++)
     */
-           
+
     for (nc = 0; getchar() != EOF; ++nc)
         ;
+        /* The body of this loop is empty, because all the work is done in the test and increment parts. But the
+        grammatical rules of C require that a for statement have a body. The isolated semicolon, called
+        a null statement, is there to satisfy that requirement. It is on a separate line to make it visible.
+        */
     printf ("%.0f\n", nc);
 }
