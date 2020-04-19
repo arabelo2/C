f10104366319ec141497fb61253372a6cce817ff
Alexandre RABELO <rabelo_alexandre@hotmail.com>
2020-04-19 15:30:08 -0300

    Update the comments

--- src/f2cwint.c
+++ src/f2cwint.c
@@ -1,15 +1,14 @@
-/* This small program converts Fahrenheit to Celsius with integer
+/* This small program converts Fahrenheit to Celsius with integer;
    Alexandre Rabelo
    April, 14, 2020   
 */
 
 #include <stdio.h>
 
-/* Print Fahrenheit-Celsius table
-   for fahr = 0, 5, 10, ..., 300
-*/
+// Print Fahrenheit-Celsius table for fahr = 0, 5, 10, ..., 300;
 
 int main(void){
+    
     int fahr, celsius, lower, upper, step;
 
     lower = 0;          // Lower limit of temperature scale
