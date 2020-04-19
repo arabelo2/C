c1370a76ad2f62b7ea2511a3fc13ccaf2be79316
Alexandre RABELO <rabelo_alexandre@hotmail.com>
2020-04-19 17:28:57 -0300

    Modify the program to work with symbolic constants

--- src/f2cwfloat.c
+++ src/f2cwfloat.c
@@ -1,24 +1,20 @@
-/* This small program converts Fahrenheit to Celsius using float;
+/* This small program converts Fahrenheit to Celsius using float and symbolic constants;
    Alexandre Rabelo
    April, 19, 2020   
 */
 
 #include <stdio.h>
 
-// Print Fahrenheit-Celsius table for fahr = 0, 5, 10, ..., 300;
+#define LOWER 0         // Lower limit of temperature scale
+#define UPPER 300       // Upper limit
+#define STEP 5          // Step size
+
+// Print Fahrenheit-Celsius table
 
 int main(void){
     
-    float fahr, celsius, lower, upper, step;
+    float fahr;
 
-    lower = 0;          // Lower limit of temperature scale
-    upper = 300;        // Upper limit
-    step = 5;          // Step size
-
-    fahr = lower;
-    while (fahr <= upper){
-        celsius = 5.0/9.0*(fahr - 32.0);
-        printf("%3.0f\t%6.2f\n", fahr, celsius);
-        fahr = fahr + step;
-    }
+    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+        printf("%3.0f\t%6.2f\n", fahr, 5.0/9.0*(fahr - 32));
 }
