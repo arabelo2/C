0dbf9f283b02031595884be7228e1abbc9e0905f
Alexandre RABELO <rabelo_alexandre@hotmail.com>
2020-04-28 17:10:05 -0300

    Create a function that raises an integer m to a positive integer power n

--- src/power.c
+++ src/power.c
@@ -11,14 +11,15 @@ int power(int m, int n);
 int main(){
     int i;
     
-    for (i = 0; i < 10; ++i)
-        printf ("%d %d %d\n", i, power(2, i), power(-3, i));
+    for (i = 0; i <= 10; ++i)
+        printf ("%10d %20d %30d\n", i, power(2, i), power(-3, i));
     return 0;
 }
 
 // Power: raise base to n-th power; n >= 0
 int power(int base, int n){
-    int i, p;
+    int i;
+    int p;
 
     p = 1;
     for (i = 1; i <= n; ++i)
